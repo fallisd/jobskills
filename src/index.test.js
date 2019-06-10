3f6c4f90f4e0d9507c69727c4f70773c90c83295
David Fallis <davidwfallis@gmail.com>
2019-06-10 13:34:01 -0700

    Made sample test

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
+import Loading from 'components/icons/Loading';
+
+it('renders loading without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Loading />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
